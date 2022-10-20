0091bbd4f326cefd09746e58615a14af143102da
sun001102 <100821736+sun001102@users.noreply.github.com>
2022-10-20 20:35:24 +0800

    DS队列

---
+++ Queue.c
@@ -0,0 +1,87 @@
+#include "Queue.h"
+void QueueInit(Queue* pq)
+{
+	assert(pq);
+	pq->head = pq->tail = NULL;
+}
+void QueueDestory(Queue* pq)
+{
+	assert(pq);
+	QNode* cur = pq->head;
+	while (cur)
+	{
+		QNode* del = cur;
+		cur = cur->next;
+		free(cur);
+	}
+}
+void QueuePush(Queue* pq, QDataType x)
+{
+	assert(pq);
+	
+	QNode* newnode = (QNode*)malloc(sizeof(QNode));//开辟创建节点
+	if (newnode == NULL)
+	{
+		perror("malloc fail");
+		exit(-1);
+	}
+	else
+	{
+		newnode->data = x;
+		newnode->next = NULL;
+	}//初始化
+	if (pq->tail== NULL)
+	{
+		pq->head = pq->tail = newnode;
+	}
+	else
+	{
+		pq->tail->next = newnode;//插入
+		pq->tail = newnode;//更新尾
+	}
+}
+void QueuePop(Queue* pq)
+{
+	assert(pq);
+	if (pq->head->next== NULL)
+	{
+		free(pq->head);
+		pq->head = pq->tail = NULL;
+	}
+	else
+	{
+		QNode* del = pq->head;//零时结点
+		pq->head = pq->head->next;//换头(保护)
+		free(del);//删头
+		del = NULL;
+	}
+}
+QDataType QueueFront(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+	return pq->head->data;
+}
+QDataType QueueBack(Queue* pq)
+{
+	assert(pq);
+	assert(!QueueEmpty(pq));
+	return pq->tail->data;
+}
+bool QueueEmpty(Queue* pq)
+{
+	assert(pq);
+	return pq->head == NULL && pq->tail == NULL;
+}
+int QueueSize(Queue* pq)
+{
+	assert(pq);
+	QNode* cur = pq->head;
+	int n = 0;
+	while (cur)
+	{
+		++n;
+		cur = cur->next;
+	}
+	return n;
+}
---
+++ Queue.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+typedef int QDataType;
+typedef struct QueueNode
+{
+	QDataType data;
+	struct QueueNode* next;
+}QNode;
+
+typedef struct Queue
+{
+	QNode* head;
+	QNode* tail;
+}Queue;
+
+void QueueInit(Queue* pq);
+void QueueDestory(Queue* pq);
+void QueuePush(Queue* pq,QDataType x);
+void QueuePop(Queue* pq);
+QDataType QueueFront(Queue* pq);
+QDataType QueueBack(Queue* pq);
+bool QueueEmpty(Queue* pq);
+int QueueSize(Queue* pq);
