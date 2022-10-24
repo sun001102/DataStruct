35c3a209ac370f5eed74dfdb31f76f5b3a6601c3
sun001102 <100821736+sun001102@users.noreply.github.com>
2022-10-24 19:48:31 +0800

    DS队列2

--- Queue.c
+++ Queue.c
@@ -1,25 +1,112 @@
+//#include "Queue.h"
+//void QueueInit(Queue* pq)
+//{
+//	assert(pq);
+//	pq->head = pq->tail = NULL;
+//}
+//void QueueDestory(Queue* pq)
+//{
+//	assert(pq);
+//	QNode* cur = pq->head;
+//	while (cur)
+//	{
+//		QNode* del = cur;
+//		cur = cur->next;
+//		free(cur);
+//	}
+//}
+//void QueuePush(Queue* pq, QDataType x)
+//{
+//	assert(pq);
+//	
+//	QNode* newnode = (QNode*)malloc(sizeof(QNode));//开辟创建节点
+//	if (newnode == NULL)
+//	{
+//		perror("malloc fail");
+//		exit(-1);
+//	}
+//	else
+//	{
+//		newnode->data = x;
+//		newnode->next = NULL;
+//	}//初始化
+//	if (pq->tail== NULL)
+//	{
+//		pq->head = pq->tail = newnode;
+//	}
+//	else
+//	{
+//		pq->tail->next = newnode;//插入
+//		pq->tail = newnode;//更新尾
+//	}
+//}
+//void QueuePop(Queue* pq)
+//{
+//	assert(pq);
+//	if (pq->head->next== NULL)
+//	{
+//		free(pq->head);
+//		pq->head = pq->tail = NULL;
+//	}
+//	else
+//	{
+//		QNode* del = pq->head;//零时结点
+//		pq->head = pq->head->next;//换头(保护)
+//		free(del);//删头
+//		del = NULL;
+//	}
+//}
+//QDataType QueueFront(Queue* pq)
+//{
+//	assert(pq);
+//	assert(!QueueEmpty(pq));
+//	return pq->head->data;
+//}
+//QDataType QueueBack(Queue* pq)
+//{
+//	assert(pq);
+//	assert(!QueueEmpty(pq));
+//	return pq->tail->data;
+//}
+//bool QueueEmpty(Queue* pq)
+//{
+//	assert(pq);
+//	return pq->head == NULL && pq->tail == NULL;
+//}
+//int QueueSize(Queue* pq)
+//{
+//	assert(pq);
+//	QNode* cur = pq->head;
+//	int n = 0;
+//	while (cur)
+//	{
+//		++n;
+//		cur = cur->next;
+//	}
+//	return n;
+//}
 #include "Queue.h"
 void QueueInit(Queue* pq)
 {
 	assert(pq);
 	pq->head = pq->tail = NULL;
 }
-void QueueDestory(Queue* pq)
+void QueueDestroy(Queue* pq)
 {
 	assert(pq);
-	QNode* cur = pq->head;
+	QNode* cur = pq->head;//临时结点
 	while (cur)
 	{
-		QNode* del = cur;
-		cur = cur->next;
-		free(cur);
+		QNode* del = cur->next;
+		free(del);
+		del = NULL;
 	}
+	pq->head = pq->tail = NULL;
 }
 void QueuePush(Queue* pq, QDataType x)
 {
 	assert(pq);
-	
-	QNode* newnode = (QNode*)malloc(sizeof(QNode));//开辟创建节点
+	QNode* newnode = (QNode*)malloc(sizeof(QNode));//开辟新结点
 	if (newnode == NULL)
 	{
 		perror("malloc fail");
@@ -29,30 +116,31 @@ void QueuePush(Queue* pq, QDataType x)
 	{
 		newnode->data = x;
 		newnode->next = NULL;
-	}//初始化
-	if (pq->tail== NULL)
+	}
+	if (pq->tail == NULL)
 	{
 		pq->head = pq->tail = newnode;
 	}
 	else
 	{
-		pq->tail->next = newnode;//插入
-		pq->tail = newnode;//更新尾
+		pq->tail->next = newnode;
+		pq->tail = newnode;
 	}
 }
 void QueuePop(Queue* pq)
 {
 	assert(pq);
-	if (pq->head->next== NULL)
+	assert(!QueueEmpty(pq));
+	if (pq->head->next == NULL)
 	{
 		free(pq->head);
 		pq->head = pq->tail = NULL;
 	}
 	else
 	{
-		QNode* del = pq->head;//零时结点
-		pq->head = pq->head->next;//换头(保护)
-		free(del);//删头
+		QNode* del = pq->head;
+		pq->head = pq->head->next;
+		free(del);
 		del = NULL;
 	}
 }
@@ -71,11 +159,12 @@ QDataType QueueBack(Queue* pq)
 bool QueueEmpty(Queue* pq)
 {
 	assert(pq);
-	return pq->head == NULL && pq->tail == NULL;
+	return pq->head == NULL;
 }
 int QueueSize(Queue* pq)
 {
 	assert(pq);
+	assert(!QueueEmpty(pq));
 	QNode* cur = pq->head;
 	int n = 0;
 	while (cur)
--- Queue.h
+++ Queue.h
@@ -1,7 +1,34 @@
+//#pragma once
+//#include <stdlib.h>
+//#include <stdbool.h>
+//#include <assert.h>
+//typedef int QDataType;
+//typedef struct QueueNode
+//{
+//	QDataType data;
+//	struct QueueNode* next;
+//}QNode;
+//
+//typedef struct Queue
+//{
+//	QNode* head;
+//	QNode* tail;
+//}Queue;
+//
+//void QueueInit(Queue* pq);
+//void QueueDestory(Queue* pq);
+//void QueuePush(Queue* pq,QDataType x);
+//void QueuePop(Queue* pq);
+//QDataType QueueFront(Queue* pq);
+//QDataType QueueBack(Queue* pq);
+//bool QueueEmpty(Queue* pq);
+//int QueueSize(Queue* pq);
 #pragma once
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+
 typedef int QDataType;
 typedef struct QueueNode
 {
@@ -11,13 +38,13 @@ typedef struct QueueNode
 
 typedef struct Queue
 {
-	QNode* head;
-	QNode* tail;
+	QNode* head;//队头指针
+	QNode* tail;//队尾指针
 }Queue;
 
 void QueueInit(Queue* pq);
-void QueueDestory(Queue* pq);
-void QueuePush(Queue* pq,QDataType x);
+void QueueDestroy(Queue* pq);
+void QueuePush(Queue* pq, QDataType x);
 void QueuePop(Queue* pq);
 QDataType QueueFront(Queue* pq);
 QDataType QueueBack(Queue* pq);
